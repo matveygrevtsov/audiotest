1811540a296aaa7bfb06e3ae03c0391764d788d4
matveygrevtsov <43029770+matveygrevtsov@users.noreply.github.com>
2021-12-22 15:06:43 +0500

    initial commit
    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function play() {
+  const audio = document.createElement('audio')
+  audio.id = 'audio'
+  audio.crossOrigin = 'anonymous'
+  audio.src = './samples/beat_guitar.mp3'
+  document.body.appendChild(audio)
+
+  const logo = document.getElementById('logo').style
+  const context = new AudioContext() // AudioContext содержит в себе методы, позволяющие контролировать и изменять аудиодорожку
+  const analyser = context.createAnalyser()
+  const src = context.createMediaElementSource(audio)
+  const array = new Float32Array(analyser.frequencyBinCount) // массив длинной, указанной в скобках
+
+  src.connect(analyser)
+  analyser.connect(context.destination)
+
+  function loop() {
+    window.requestAnimationFrame(loop)
+    analyser.getFloatFrequencyData(array)
+    logo.minHeight = logo.minWidth = `${array[40]}px`
+  }
+
+  audio.play()
+  loop()
+}
