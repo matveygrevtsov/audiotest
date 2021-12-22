0fab9b1f7fd5dcf9cdf1df9ae34210369fd15b0a
matveygrevtsov <43029770+matveygrevtsov@users.noreply.github.com>
2021-12-22 15:43:41 +0500

    commit6
    commit6

--- script.js
+++ script.js
@@ -9,15 +9,15 @@ function play() {
   const context = new AudioContext() // AudioContext содержит в себе методы, позволяющие контролировать и изменять аудиодорожку
   const analyser = context.createAnalyser()
   const src = context.createMediaElementSource(audio)
-  const array = new Float32Array(analyser.frequencyBinCount) // массив длинной, указанной в скобках
+  const array = new Uint8Array(analyser.frequencyBinCount) // массив длинной, указанной в скобках
 
   src.connect(analyser)
   analyser.connect(context.destination)
 
   function loop() {
     window.requestAnimationFrame(loop)
-    analyser.getFloatFrequencyData(array)
-    const value = Math.min(Math.abs(array[40]), 1000)
+    analyser.getByteFrequencyData(array)
+    const value = array[40]
     console.log(value)
     logo.style = `width: ${value}px; height: ${value}px`
   }
